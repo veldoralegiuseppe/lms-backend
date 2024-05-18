c45067eda70b358afa2fb499f8ea43dd834ab857
veldoralegiuseppe <veldorale.giuseppe@gmail.com>
2024-05-18 18:13:42 +0200

    Create sessione

--- src/main/java/com/ecampus/lms/dto/response/SessioneDTO.java
+++ src/main/java/com/ecampus/lms/dto/response/SessioneDTO.java
@@ -2,5 +2,9 @@ package com.ecampus.lms.dto.response;
 
 import jakarta.validation.constraints.NotBlank;
 
-public record SessioneDTO(Integer id, @NotBlank String nomeCorso, @NotBlank String data, @NotBlank  String tipo, DocumentaleDTO provaScritta) {
+public record SessioneDTO(Integer id,
+                          @NotBlank String nomeCorso,
+                          @NotBlank String data,
+                          @NotBlank  String tipo,
+                          DocumentaleDTO provaScritta) {
 }
